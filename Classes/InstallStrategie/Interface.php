fb5995f668d8a0db4e833de0b25b7e2764cc104f
Michael Klapper <development@morphodo.com>
2011-10-31 11:23:15 +0100

    - Remove files with typo

--- Classes/InstallStrategie/Interface.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-interface EasyDeploy_InstallStrategie_Interface {
-	public function installSteps($packageDeliveryFolder, $packageFileName, EasyDeploy_DeployService $deployService, EasyDeploy_AbstractServer $server);
-}
