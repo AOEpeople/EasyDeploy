14183faa57ad34a9227b267140d9a576aa6afc8b
Michael Klapper <michael.klapper@WS-040.aoemedia.lan>
2012-03-07 10:34:58 +0100

    Add optional --silent mode

--- Classes/InstallStrategy/PHPInstaller.php
+++ Classes/InstallStrategy/PHPInstaller.php
@@ -4,12 +4,17 @@
  * Default Installer Strategie for the PHP Installer: Just executes the installscript that is delivered within the package
  */
 class EasyDeploy_InstallStrategy_PHPInstaller implements EasyDeploy_InstallStrategy_Interface {
-	
+
 	/**
 	 * @var boolean
 	 */
-	private $createBackupBeforeInstalling = true;
-	
+	private $createBackupBeforeInstalling = TRUE;
+
+	/**
+	 * @var boolean
+	 */
+	private $silentMode = FALSE;
+
 	/**
 	 * @var string
 	 */
@@ -22,13 +27,19 @@ class EasyDeploy_InstallStrategy_PHPInstaller implements EasyDeploy_InstallStrat
 	 * @param EasyDeploy_AbstractServer $server
 	 */
 	public function installSteps($packageDeliveryFolder, $packageFileName, EasyDeploy_DeployService $deployService, EasyDeploy_AbstractServer $server) {
-		// fix permissions
-		$server->run('chmod -R ug+x '.$packageDeliveryFolder.'/'.$packageFileName.'/installbinaries');
-		// install package
 		$additionalParameters = '';
-		if ($this->createBackupBeforeInstalling) {
-			$additionalParameters =' --createNewMasterBackup=1';
+			// fix permissions
+		$server->run('chmod -R ug+x '.$packageDeliveryFolder.'/'.$packageFileName.'/installbinaries');
+
+		if ($this->createBackupBeforeInstalling === TRUE) {
+			$additionalParameters .=' --createNewMasterBackup=1';
 		}
+
+		if ($this->silentMode === TRUE) {
+			$additionalParameters .=' --silent';
+		}
+
+			 // install package
 		$server->run($this->phpbinary . ' ' . $packageDeliveryFolder . '/' . $packageFileName . '/installbinaries/install.php \
 			--systemPath="' . $this->getSystemPath( $deployService )  . '" \
 			--backupstorageroot="' . $deployService->getBackupstorageroot() . '" \
@@ -42,23 +53,31 @@ class EasyDeploy_InstallStrategy_PHPInstaller implements EasyDeploy_InstallStrat
 			print EasyDeploy_Utils::formatMessage('PHP binary '.$bin.' does not exist or is not executable.', EasyDeploy_Utils::MESSAGE_TYPE_WARNING);
 		}
 	}
-	
+
 	/**
 	 * Default is set to true
-	 * 
+	 *
 	 * @depreciated this is a concept of the install strategy - you should pass a initialised strategie
-	 * 
+	 *
 	 * @param boolean $createBackup
 	 * @return void
 	 */
 	public function setCreateBackupBeforeInstalling($createBackup) {
 		$this->createBackupBeforeInstalling = (boolean) $createBackup;
 	}
-	
-	
+
+	/**
+	 * Set this flag to force the installation without any confirmation.
+	 *
+	 * @param boolean $activate
+	 */
+	public function setSilentMode($activate) {
+		$this->silentMode = $activate;
+	}
+
 	/**
 	 * Gets relevant system path (path for installing the package) based on the infos in the deployservice
-	 * 
+	 *
 	 * @param EasyDeploy_DeployService $deployService
 	 * @return string
 	 */
